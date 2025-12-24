976c0c2a565526537c284835bffeeccf1cc120e7
Tejeswar23 <polakatejeswarreddy@gmail.com>
2025-12-24 16:16:40 +0530

    To Main

---
+++ src/vechile.py
@@ -0,0 +1,8 @@
+class UrbanMobility:
+    def __init__(self):
+        pass
+    def greet(self):
+        print("Welcome to Eco-Ride Urban Mobility System")
+        
+um = UrbanMobility()
+um.greet()
